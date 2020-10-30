b280779822fab8638b746850b884332888dbdf37
Jason Ray <jasonray@jayray.net>
2020-10-30 19:41:35 -0400

    add perc

---
+++ Percolation.java
@@ -0,0 +1,34 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+    // creates n-by-n grid, with all sites initially blocked
+    public Percolation(int n) {
+        WeightedQuickUnionUF qf = new WeightedQuickUnionUF();
+    }
+
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col) {
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        return false;
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        return false;
+    }
+
+    // returns the number of open sites
+    public int numberOfOpenSites() {
+
+    }
+
+    public boolean percolates() {
+        return false;
+    }
+
+}
