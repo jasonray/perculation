3eaffea7c2f3a9da75a318cecdc9c99290e82530
Jason Ray <jasonray@jayray.net>
2020-10-30 21:30:49 -0400

    update constructor to track qf

--- Percolation.java
+++ Percolation.java
@@ -1,22 +1,33 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private int virtualTopIndex;
+    private int virtualBottomIndex;
     private WeightedQuickUnionUF qf;
     private boolean[][] siteState;
+    private int[][] qfIndex;
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
         siteState = new boolean[n][n];
+        qfIndex = new int[n][n];
 
+        int index = 0;
         for (int row = 0; row < n; row++) {
             for (int col = 0; col < n; col++) {
                 siteState[row][col] = false;
+                qfIndex[row][col] = index;
+                index++;
             }
         }
 
+        virtualTopIndex = index;
+        index++;
+        virtualBottomIndex = index;
+
         // the extra +2 is for tracking the virtual top and virtual bottom
         int numberOfCells = n * n + 2;
-        qf = new WeightedQuickUnionUF(n * n);
+        qf = new WeightedQuickUnionUF(numberOfCells);
     }
 
     // opens the site (row, col) if it is not open already
