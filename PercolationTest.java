a109f5fffe44f96487ebcbe7914e2b40c360c2bf
Jason Ray <jasonray@jayray.net>
2020-10-30 20:51:26 -0400

    implement site state

--- PercolationTest.java
+++ PercolationTest.java
@@ -28,4 +28,17 @@ class PercolationTest {
         perc.open(0, 0);
         assertEquals(true, perc.isOpen(0, 0));
     }
+
+    @Test
+    public void switchSiteFromOpenToCloseVerifyOpenN2() {
+        Percolation perc = new Percolation(2);
+        assertEquals(false, perc.isOpen(0, 0));
+        assertEquals(false, perc.isOpen(1, 1));
+        perc.open(0, 0);
+        assertEquals(true, perc.isOpen(0, 0));
+        assertEquals(false, perc.isOpen(1, 1));
+        perc.open(1, 1);
+        assertEquals(true, perc.isOpen(0, 0));
+        assertEquals(true, perc.isOpen(1, 1));
+    }
 }
