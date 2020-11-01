229f8601b0ef5918ea6162985b1a343bbcb54b77
Jason Ray <jasonray@jayray.net>
2020-11-01 12:57:14 -0500

    read params from commandline, switch random

--- PercolationStats.java
+++ PercolationStats.java
@@ -4,15 +4,29 @@
  *  Last modified:     1/1/2019
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdRandom;
+
 public class PercolationStats {
+    private int n;
     private int size;
     private int numberOfSimulationsThatHaveBeenRun = 0;
     private double[] results;
 
     // test client (see below)
     public static void main(String[] args) {
-        int n = 2;
-        int trials = 4;
+        int n;
+        int trials;
+
+        if (args.length == 2) {
+            n = Integer.parseInt(args[0]);
+            trials = Integer.parseInt(args[1]);
+        }
+        else {
+            n = 3;
+            trials = 10;
+        }
+        System.out.println("n:" + n);
+        System.out.println("t:" + trials);
 
         PercolationStats simulator = new PercolationStats(n, trials);
     }
@@ -22,24 +36,25 @@ public class PercolationStats {
         if (n <= 0) throw new IllegalArgumentException("invalid size");
         if (trials <= 0) throw new IllegalArgumentException("invalid number of trials");
 
+        this.n = n;
         this.size = n * n;
         this.results = new double[trials];
 
         for (int trialNumber = 1; trialNumber <= trials; trialNumber++) {
-            System.out.println("begin simulation " + trialNumber);
+            // System.out.println("begin simulation " + trialNumber);
             runOneSimulation(n);
         }
     }
 
     private void runOneSimulation(int n) {
         Percolation perc = new Percolation(n);
-        java.util.Random random = new java.util.Random();
 
         int loopNumber = 0;
         do {
             loopNumber++;
-            int nextRow = random.nextInt(n) + 1;
-            int nextCol = random.nextInt(n) + 1;
+            int nextRow = StdRandom.uniform(n) + 1;
+            int nextCol = StdRandom.uniform(n) + 1;
+            // System.out.println("open [" + nextRow + ", " + nextCol + "]");
             perc.open(nextRow, nextCol);
         } while (!perc.percolates());
 
@@ -52,10 +67,7 @@ public class PercolationStats {
         this.results[numberOfSimulationsThatHaveBeenRun] = percentageOfOpenSites;
         this.numberOfSimulationsThatHaveBeenRun++;
 
-        System.out.println(
-                "simulation complete [" + percentageOfOpenSites + "][" + this.mean() + "][" + this
-                        .stddev() + "][ " + this.confidenceLo() + " - " + this.confidenceHi()
-                        + "]");
+        this.outputStats();
     }
 
     // sample mean of percolation threshold
@@ -65,6 +77,22 @@ public class PercolationStats {
             sumOfResults = sumOfResults + this.results[i];
         }
         return sumOfResults / this.numberOfSimulationsThatHaveBeenRun;
+    }
+
+    public double max() {
+        double trackingMax = results[0];
+        for (int i = 0; i < this.numberOfSimulationsThatHaveBeenRun; i++) {
+            trackingMax = Math.max(trackingMax, this.results[i]);
+        }
+        return trackingMax;
+    }
+
+    public double min() {
+        double trackingMax = results[0];
+        for (int i = 0; i < this.numberOfSimulationsThatHaveBeenRun; i++) {
+            trackingMax = Math.min(trackingMax, this.results[i]);
+        }
+        return trackingMax;
     }
 
     // sample standard deviation of percolation threshold
@@ -97,6 +125,19 @@ public class PercolationStats {
             return this.mean() + rightSide;
         else
             return this.mean() - rightSide;
+    }
+
+    public void outputStats() {
+        System.out.println("Matrix Size       : " + this.n);
+        System.out.println("Trials            : " + this.numberOfSimulationsThatHaveBeenRun);
+        System.out.println("mean              : " + this.mean());
+        System.out.println("stddev            : " + this.stddev());
+        System.out.println(
+                "range             : " + "[" + this.min() + ", " + this
+                        .max() + "]");
+        System.out.println(
+                "95% confidence    : " + "[" + this.confidenceLo() + ", " + this
+                        .confidenceHi() + "]");
     }
 
 }
